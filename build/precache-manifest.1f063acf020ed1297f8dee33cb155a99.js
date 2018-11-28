5d81521889bde2c6bb7db649a9730bb66bc9ee12
wgj <email@qqcom>
2018-11-28 20:10:10 +0800

    打包项目

---
+++ build/precache-manifest.1f063acf020ed1297f8dee33cb155a99.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "99617960a8ad92227956",
+    "url": "./static/js/main.99617960.chunk.js"
+  },
+  {
+    "revision": "49d68f4d20e37a3e604c",
+    "url": "./static/js/1.49d68f4d.chunk.js"
+  },
+  {
+    "revision": "99617960a8ad92227956",
+    "url": "./static/css/main.a2971c85.chunk.css"
+  },
+  {
+    "revision": "33c6cb8adef236d069c2fb68ebc40f26",
+    "url": "./index.html"
+  }
+];
